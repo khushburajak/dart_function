e9aaf27a95faf3ed488e9c3c13bcc6a5bf16d6e0
khushburajak <khushburajak17@gmail.com>
2024-11-19 23:18:24 +0545

    odd and even

---
+++ odd_even.dart
@@ -0,0 +1,12 @@
+// Write a dart program to check if the number is odd or even.
+void main() {
+  EvenOdd(5);
+}
+
+void EvenOdd(int n) {
+  if (n % 2 == 0) {
+    print("the number is even");
+  } else {
+    print("The number is odd");
+  }
+}
